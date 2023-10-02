a3e6518613f720df65bd8aed21fdf68629ca5904
Soujanya Bhowmick <soujanyawork600@gmail.com>
2023-10-02 21:27:32 +0300

    new codes and git ignore

---
+++ num_half_pyramid.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"enter the number ";
+    cin>>n;
+
+    for(int row=0;row<5;row++){
+        for(int col=0;col<row+1;col++){
+            cout<<"* ";
+     
+       }
+    cout<<endl;
+    }
+}
