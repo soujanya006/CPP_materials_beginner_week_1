7bd2c0753c4fb3456c364ed96c85409c8955daea
Soujanya Bhowmick <soujanyawork600@gmail.com>
2023-10-03 01:22:21 +0300

    new codes

--- half_inverted_pyramid_pat.cpp
+++ half_inverted_pyramid_pat.cpp
@@ -11,6 +11,7 @@ int main(){
         for(int col=0;col<n-row;col++){
             cout<<"*";
 
+
         }
     cout<<endl;
     }
--- num_half_pyramid.cpp
+++ num_half_pyramid.cpp
@@ -11,6 +11,7 @@ int main(){
         for(int col=0;col<row+1;col++){
             cout<<"* ";
      
+     
        }
     cout<<endl;
     }
